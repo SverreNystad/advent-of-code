110111ab3b8bb7fe96f58ae40bd39670e2425723
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-12-02 01:32:08 +0100

    Refactor: Add code to functions to be used in part two

--- 2023/Day01/part_1.py
+++ 2023/Day01/part_1.py
@@ -3,21 +3,26 @@ file_name = "input.txt"
 
 # Read file input.txt and store it in a list
 with open(file_name, "r") as f:
-    input = f.readlines()
+    data = f.readlines()
 
 
-    solution = []
-    # Remove all non numeric characters
-    lines = [re.sub(r'[^0-9]', '', line) for line in input]
+def remove_non_numeric_characters(raw_lines: str):
+    """Removes all non numeric characters from a list of strings"""
+    return [re.sub(r'[^0-9]', '', line) for line in raw_lines]
 
+def find_first_and_last_number(lines: str):
+    numbers = []
     # Loop through each line and add the first and last number
     for line in lines:
         first_num = line[0]
         last_num = line[-1]
-        solution.append(first_num + last_num)
+        numbers.append(first_num + last_num)
 
+    total = sum([int(i) for i in numbers])
+    return total
 
 if __name__ == "__main__":
     # find the sum of all numbers in the list
-    total = sum([int(i) for i in solution])
-    print(total)
+    sanitized_data = remove_non_numeric_characters(data)
+    solution = find_first_and_last_number(sanitized_data)
+    print(solution)
